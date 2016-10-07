c8f226cf8fc1bf1953315d86bef9906c93f963c9
Érico de Souza Loewe <ed-lowe@hotmail.com>
2016-10-06 21:24:06 -0300

    improved thinks

--- escalonador/Form1.cs
+++ escalonador/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace escalonador
@@ -71,7 +64,7 @@ namespace escalonador
             }
         }
 
-        public void limpaTela() {
+        public void LimpaTela() {
             txtDescricao.Text = "";
             comboStatus.SelectedIndex = 0;
             txtTempo.Text = "";
--- escalonador/Processo.cs
+++ escalonador/Processo.cs
@@ -1,96 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-
-namespace escalonador
+﻿namespace escalonador
 {
-
-
-    class Processo
-    {        
-        public int ordem
-        {
-            get
-            {
-                return ordem;
-            }
-            set
-            {
-                this.ordem = ordem;
-            }
-        }
-        public string descricao
-        {
-            get
-            {
-                return descricao;
-            }
-            set
-            {
-                this.descricao = descricao;
-            }
-
-        }
-        public int tempo
-        {
-            get
-            {
-                return tempo;
-            }
-            set
-            {
-                this.tempo = tempo;
-            }
-
-        }
-        public string status
-        {
-            get
-            {
-                return status;
-            }
-            set
-            {
-                this.status = status;
-            }
-        }
-        public string processador
-        {
-            get
-            {
-                return processador;
-            }
-            set
-            {
-                this.processador = processador;
-            }
-        }
-        public int lala { get; set; }
-        public static Thread run()
-        {
-            while (true)
-            {
-
-            }
-        }
-
-        public void teste1()
-        {
-            var thread = new Thread(new TesteT().teste());
-
-            thread.Start();
-
-        }
-    }
-
-    public class TesteT
+    public class Processo
     {
-        public ThreadStart teste()
-        {
-            throw new NotImplementedException();
-        }
+        public int Ordem { get; set; }
+        public string Descricao { get; set; }
+        public int Tempo { get; set; }
+        public string Status { get; set; }
+        public string Processador { get; set; }
     }
 }
